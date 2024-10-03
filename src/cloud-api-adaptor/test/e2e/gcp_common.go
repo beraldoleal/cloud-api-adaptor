d946ee1ba14d9f748e774c232614be7c57e8b7b1
Beraldo Leal <bleal@redhat.com>
2024-10-03 14:07:23 -0400

    gcp: adding basic e2e tests
    Those are just a basic set of sets, more to come in future PRs.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

---
+++ src/cloud-api-adaptor/test/e2e/gcp_common.go
@@ -0,0 +1,56 @@
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"testing"
+	"time"
+
+	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner/gcp"
+)
+
+// GCPAssert implements the CloudAssert interface.
+type GCPAssert struct {
+	Vpc *pv.GCPVPC
+}
+
+func NewGCPAssert() GCPAssert {
+	return GCPAssert{
+		Vpc: pv.GCPProps.GcpVPC,
+	}
+}
+
+func (aa GCPAssert) DefaultTimeout() time.Duration {
+	return 1 * time.Minute
+}
+
+func (aa GCPAssert) HasPodVM(t *testing.T, id string) {
+    podvmPrefix := "podvm-" + id
+
+    // Create a request to list instances in the specified project and zone.
+    req := aa.ComputeService.Instances.List(aa.ProjectID, aa.Zone)
+    instances, err := req.Do()
+    if err != nil {
+        t.Errorf("Failed to list instances: %v", err)
+        return
+    }
+
+    found := false
+    for _, instance := range instances.Items {
+        if instance.Status != "TERMINATED" {
+            if strings.HasPrefix(instance.Name, podvmPrefix) {
+                found = true
+                break
+            }
+        }
+    }
+
+    if !found {
+        t.Errorf("Podvm name=%s not found", id)
+    }
+}
+
+func (aa GCPAssert) GetInstanceType(t *testing.T, podName string) (string, error) {
+	return "", nil
+}
